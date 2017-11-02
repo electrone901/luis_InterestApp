0c87368d0b13084c623d90fbc692f2d513a6ff8f
GitBubBoddy <kjmei4gdatabase@gmail.com>
2017-11-01 21:00:43 -0400

    Finished Styling Login Page

--- App.js
+++ App.js
@@ -13,6 +13,9 @@ import {
   StatusBar
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import { FormLabel, FormInput } from 'react-native-elements';
+//import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 // IMPORT COMPONENTS
@@ -86,28 +89,44 @@ class LoginFormScreen extends Component {
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-            <Button onPress={() => navigate('Interest')} title="INTEREST PAGE" />
-            <TextInput style = {styles.input}
+
+            <Button  
+                 large
+                 onPress={() => navigate('Interest')} 
+                 buttonStyle={{backgroundColor:'#2980b6'}}
+                 textStyle={{textAlign: 'center'}}
+                 //icon={{name: 'star-half-full', type: 'font-awesome', color:'#D4AF37', size:32}}
+                 title="INTEREST PAGE"                 
+                 
+                 />
+            <Text style={styles.Heading}>Email or Mobile Num</Text>
+            <FormInput style = {styles.input}
+                        underlineColorAndroid="white"
                         autoCapitalize="none"
                         onSubmitEditing={() => this.passwordInput.focus()}
                         autoCorrect={false}
                         keyboardType='email-address'
                         returnKeyType="next"
-                        placeholder='Email or Mobile Num'
+                        placeholder='Please enter your email here..'
                         onChangeText ={(email) => this.setState({email})}
                         placeholderTextColor='rgba(225,225,225,0.7)'/>
-
-            <TextInput style = {styles.input}
+            
+            <Text style={styles.Heading}>Password</Text>
+            <FormInput style = {styles.input2}
                        returnKeyType="go" ref={(input)=> this.passwordInput = input}
-                       placeholder='Password'
+                       placeholder='Please enter your password here..'
                        onChangeText ={(password) => this.setState({password})}
                        placeholderTextColor='rgba(225,225,225,0.7)'
                        secureTextEntry/>
-          <TouchableOpacity style={styles.buttonContainer} onPress={this.signup}>
-                <Text  style={styles.buttonTextSignUp}>Signup</Text>
+          <TouchableOpacity style={styles.buttonContainer2} onPress={this.signup}>
+                <Icon name="home" style={styles.icon1} size={25}>
+                <Text  style={styles.buttonTextSignUp}>  Start Now</Text>
+                </Icon>
             </TouchableOpacity>
             <TouchableOpacity style={styles.buttonContainer} onPress={this.login}>
-                <Text  style={styles.buttonText}>Login</Text>
+                <Icon name="sign-in" style={styles.icon1} size={25}>
+                <Text  style={styles.buttonTextSignUp}>  Login</Text>
+                </Icon>
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.buttonContainerGo}  onPress={() => navigate('Interest')}>
@@ -166,18 +185,67 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#3498db',
+
+    },
+    Heading:{
+     fontSize:12,
+     color:'white',
+     marginLeft:17,
+     //marginTop:20,
+     paddingTop:10,
     },
     input:{
         height: 40,
         backgroundColor: 'rgba(225,225,225,0.2)',
-        marginBottom: 10,
+        //marginTop:1,
+        //marginBottom:5,
         padding: 10,
         color: '#fff'
     },
-    buttonContainer:{
+    input2:{
+        height: 40,
+        backgroundColor: 'rgba(225,225,225,0.2)',
+        marginBottom:10,
+        padding: 10,
+        color: '#fff'
+    },
+    icon1:{
+        height:45,
+        //width:45,
+        color:'white',
+        padding: 10,
+        marginLeft:140,
+        //marginRight:50,
+        justifyContent: 'center',
+
+    },
+    buttonContainer2:{
+
+        height:50,
+        borderColor: '#2980b6',
+        borderWidth: 1,
+        borderRadius: 100,
+        //alignSelf: 'stretch',
         backgroundColor: '#2980b6',
-        paddingVertical: 15,
+        //paddingVertical: 15,
         marginBottom: 5,
+        marginTop:30,
+        justifyContent: 'center',
+        //alignItems: 'center',
+    },
+    buttonContainer:{
+
+        height:50,
+        borderColor: '#2980b6',
+        borderWidth: 1,
+        borderRadius: 100,
+        //alignSelf: 'stretch',
+        backgroundColor: '#2980b6',
+        //paddingVertical: 15,
+        marginBottom: 5,
+
+        justifyContent: 'center',
+        //alignItems: 'center',
     },
     buttonContainerGo:{
         backgroundColor: '#06b3e2',
@@ -188,13 +256,16 @@ const styles = StyleSheet.create({
         color: '#fff',
         textAlign: 'center',
         // fontWeight: '700',
+        fontFamily:'font-awesome',
         paddingBottom:10
     },
     buttonTextSignUp:{
         color: 'orange',
-        textAlign: 'center',
+        fontSize:15,
+        //textAlign: 'center',
         // fontWeight: '700',
-        paddingBottom:10
+        marginLeft:100,
+        marginBottom:50,
     },
 
     loginButton:{
